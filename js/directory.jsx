e703af280a1771a2385d6ae2d3bf677e2c4ee7c9
khalid2424 <ele21004@byui.edu>
2022-03-18 17:11:26 +0100

    commit

--- js/directory.jsx
+++ js/directory.jsx
@@ -1,4 +1,4 @@
-const requestURL = "https://khalid2424.github.io/wdd230/chamber/data/data.json";
+const requestURL = "https://khalid2424.github.io/chamber/data/data.json";
 const cards = document.querySelector('.cards');
 
 fetch(requestURL)
