c2464ad4390ac1fcb7b3baef37cb4fdba2133908
khalid2424 <ele21004@byui.edu>
2022-03-18 14:49:45 +0100

    updated JSON file

--- js/directory.jsx
+++ js/directory.jsx
@@ -1,4 +1,4 @@
-const requestURL = "https://khalid2424.github.io/wdd230/chamber/data/data.json";
+const requestURL = "https://github.com/Khalid2424/chamber/blob/main/data/data.json";
 const cards = document.querySelector('.cards');
 
 fetch(requestURL)
